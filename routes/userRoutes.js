b046e0f8772d43300314346ef602650ca97c5b24
Vijay Maurya <126171677+vjzest@users.noreply.github.com>
2026-09-26 17:30:30 +0530

    Fix ReferenceError: import addProjectReview in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,7 @@ const {
   getAllContractorProjects,
   getContractorBySEO,
   getSellerBySEO,
+  addProjectReview,
 } = require("../controllers/userController");
 
 const upload = require("../middleware/uploadMiddleware");
